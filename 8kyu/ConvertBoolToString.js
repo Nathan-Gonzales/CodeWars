c04d74c088f2e44ecb6c7eb9d74a01191288c494
Nathan-Gonzales <nathangonzales114@yahoo.com>
2022-05-19 22:57:20 -0600

    6th CodeWars Problem

---
+++ 8kyu/ConvertBoolToString.js
@@ -0,0 +1,20 @@
+/* Link to problem: https://www.codewars.com/kata/551b4501ac0447318f0009cd/train/javascript
+
+   Problem:     Convert a Boolean to a String
+
+   Description: Implement a function which convert the given boolean value into its string representation.
+
+   Note: Only valid inputs will be given.
+
+
+   Input -> Output Examples: 
+    true  --> "true"
+    false --> "false"
+*/
+
+
+// Solution
+function booleanToString(b){
+    if(b) { return "true"}
+    if(!b){ return "false"}
+  }
