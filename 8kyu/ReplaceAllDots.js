7133b66135ecce5e2dd72ad61c0a8202c1e5598d
Nathan-Gonzales <nathangonzales114@yahoo.com>
2022-06-25 20:25:04 -0600

    replace all dots problem

---
+++ 8kyu/ReplaceAllDots.js
@@ -0,0 +1,23 @@
+/* Link to problem: https://www.codewars.com/kata/596c6eb85b0f515834000049/train/javascript
+
+   Problem:     FIXME: Replace all dots
+
+   Description: The code provided is supposed replace all the dots . in the specified String str with dashes -
+   But it's not working properly.
+
+   Task
+   Fix the bug so we can all go home early.
+
+   Notes
+   String str will never be null.
+
+
+   Input -> Output Examples: 
+    "one.two.three" -> "one-two-three"
+*/
+
+
+// Solution
+var replaceDots = function(str) {
+    return str.replace(/\./g, '-');
+}
